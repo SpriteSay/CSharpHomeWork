a6a11d9b8427ba1eccab38353042f6f81d1d1691
SpriteSay <793750155@qq.com>
2020-02-29 12:11:29 +0800

    第二周作业，素数因子与埃氏筛法等三个项目

---
+++ Week 2-Prime/Task1/Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime
+{
+    class Program
+    {
+        //埃式筛法判断素数
+        public bool IsPrime(int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个数据：");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            Program pg = new Program();
+            if (a >= 2)
+            {
+                Console.WriteLine("它的素数因子是：");
+                for (int i = 2; i <= a; i++)
+                {
+                    if (pg.IsPrime(i) && a % i == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("您必须输入一个大于1的自然数");
+
+            }
+        }
+    }
+}
+
---
+++ Week 2-Prime/Task3/Task3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        public void Calculate(bool[] isPrime)
+        {
+            for (int d = 2; d <= 100; d++)
+            {
+                for (int i = 2; i <= 100; i++)
+                {
+                    if (i % d == 0 && isPrime[i - 1] == true && i != d)
+                    {
+                        isPrime[i - 1] = false;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+            //初始化数组
+            bool[] isPrime = new bool[100];
+            for (int i = 0; i < 100; i++)
+            {
+                isPrime[i] = true;
+            }
+
+            Console.WriteLine("2-100内的素数有：");
+            //埃氏筛选
+            pg.Calculate(isPrime);
+            
+            //输出数据
+            for (int i = 2; i < 100; i++)
+            {
+                if(isPrime[i-1] == true)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+        }
+    }
+}
